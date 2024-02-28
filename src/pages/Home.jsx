440587a0ad8aacce4de21931bb46d69b460e518d
axllema <axellexm@gmail.com>
2024-02-28 18:19:04 +0100

    creation of login & user pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'; 
+// import Header from '../components/header';
+// import Banner from '../components/banner';
+// import Footer from '../components/footer';
+// import '../scss/style.scss'
+
+function Home() {
+    return (
+        <div>
+            <h1>Bienvenue sur la page d'accueil!</h1>
+            <p>Ceci est un exemple de texte ajouté à la page d'accueil.</p>
+        </div>
+    );
+}
+
+export default Home;
