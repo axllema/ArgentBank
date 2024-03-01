4ff9d00bea76ee13aeda6be1df7315fa25f73081
axllema <axellexm@gmail.com>
2024-03-01 23:53:48 +0100

    création de userheader components, userpofile jsx & userprofile scss

---
+++ src/components/userheader.jsx
@@ -0,0 +1,12 @@
+import '../scss/pages/_userProfile.scss'
+import '../scss/style.scss'
+
+const UserHeader = () => {
+    return (
+    <div className="header">
+        <h1>Welcome back</h1>
+    </div>
+    );
+    };
+
+export default UserHeader;
